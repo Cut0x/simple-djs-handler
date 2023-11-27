25dbab8b2dc48a81b0d64c76f6b74d2901fb29c9
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-11-27 03:07:41 +0100

    Update App.js
    Add possibility to see the module version

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ const { BotClient } = require("./src/ConstructorAction/BotClient");
 const { BotEvent } = require("./src/ConstructorAction/BotEvent");
 const { BotCommand } = require("./src/ConstructorAction/BotCommand");
 
+const { version } = require("./package.json")
+
 module.exports = {
     BotClient,
     BotEvent,
-    BotCommand
+    BotCommand,
+    version
 };
