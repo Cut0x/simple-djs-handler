ffbf6ea8a33aba601526267c5c29e19ff5774df5
Cut0x <71967731+Cut0x@users.noreply.github.com>
2024-08-18 03:55:28 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 const { BotClient } = require("./src/ConstructorAction/BotClient");
 const { BotEvent } = require("./src/ConstructorAction/BotEvent");
 const { BotCommand } = require("./src/ConstructorAction/BotCommand");
-const { CreateEmbed } = require("./src/ConstructorAction/CreateEmbed");
 
 const { version } = require("./package.json")
 
